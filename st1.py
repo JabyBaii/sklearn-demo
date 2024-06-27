66a27afaae4b6882d41bdf663a54f03210ec894c
Iai <iai-0001@outlook.com>
2024-06-27 08:57:03 +0800

    tfidf

--- st1.py
+++ st1.py
@@ -1,7 +1,7 @@
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction import DictVectorizer
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 import jieba
 
 
@@ -122,6 +122,31 @@ def cut_words2():
     return None
 
 
+def tfidf_demo():
+    """
+    使用TF-IDF进行文本特征抽取
+    :return:
+    """
+    # 将中文文本进行分词
+    data = [
+        "one to one, 今天很残酷，明天更残酷，后天很美好，但是绝大部分人是死在明天晚上，所以每个人都不要放弃今天",
+        "我们看到的从很远星系来的光是几百万年之前发出的，这样当我们看到宇宙时，我们是在看他的过去",
+        "如果只用一种方式了解魔种事物，你就不会真正了解它，了解事物真正含义的奥秘就在于如何将其与我们所了解到的事物相联系"
+    ]
+    data_new = []
+    for sentence in data:
+        data_new.append(cut_words(sentence))
+    # print(data_new)
+
+    # 特征值提取
+    transfer = TfidfVectorizer(stop_words=["一种", "所以"])
+    data_new2 = transfer.fit_transform(data_new)
+    print("data_new: \n", data_new2.toarray())  # 转成字典格式
+    print("feature name: \n", transfer.get_feature_names_out())
+
+    return None
+
+
 if __name__ == '__main__':
     # code1 the using of sklearn dataset
     # datasets_iris_demo()
@@ -138,7 +163,12 @@ if __name__ == '__main__':
     # code5 zh text feature_extraction, cut words by person
     # zh_text_count_demo2()
 
-    # code6 use obj to cut words
+    # code6 use lib to cut words
     # print(cut_words("我爱北京天安门"))
 
-    cut_words2()
+    # code7
+    # cut_words2()
+
+    # code8
+    tfidf_demo()
+
