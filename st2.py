2fda31e9dc72ff90c6385ff497f26b02f5897f4e
Iai <iai-0001@outlook.com>
2024-06-29 02:04:24 +0800

    add交叉验证集网格搜索

--- st2.py
+++ st2.py
@@ -2,6 +2,7 @@ from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import GridSearchCV
 
 
 def knn_iris():
@@ -36,6 +37,50 @@ def knn_iris():
     return None
 
 
+def knn_iris_gscv():
+    """
+    用knn对鸢尾花进行分类，添加交叉验证集网格搜索
+    :return:
+    """
+    # 1) 获取数据
+    iris = load_iris()
+
+    # 2) 划分数据集
+    x_train, x_test, y_train, y_test = train_test_split(iris.data, iris.target, random_state=6)
+
+    # 3) 特征工程：标准化
+    transfer = StandardScaler()
+    x_train = transfer.fit_transform(x_train)
+    x_test = transfer.transform(x_test)
+
+    # 4) KNN算法预估器
+    estimator = KNeighborsClassifier()
+
+    # 加入网格搜索与交叉验证
+    param_dict = {"n_neighbors": [1, 3, 5, 7, 9, 11, 13]}
+    estimator = GridSearchCV(estimator, param_grid=param_dict, cv=10)
+
+    estimator.fit(x_train, y_train)
+
+    # 5) 模型评估
+    # 方法1：直接比对真实值和预测值
+    y_predict = estimator.predict(x_test)
+    print("y_predict: \n", y_predict)
+    print("真实值和预测值的比对结果：\n", y_test == y_predict)
+    # 方法2：计算准确率
+    score = estimator.score(x_test, y_test)
+    print("准确率为：\n", score)
+
+    print("最佳参数：\n", estimator.best_params_)
+    print("最佳结果：\n", estimator.best_score_)
+    print("最佳估计器：\n", estimator.best_estimator_)
+    print("交叉验证结果：\n", estimator.cv_results_)
+
+    return None
+
+
 if __name__ == '__main__':
     # case1
-    knn_iris()
+    # knn_iris()
+    # case2
+    knn_iris_gscv()
